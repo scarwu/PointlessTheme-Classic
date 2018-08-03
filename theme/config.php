f9263bc1ae0dd40a919b42d7c02c13a29c21fdb9
Scar Wu <xneriscool@gmail.com>
2018-08-03 14:07:52 +0800

    1. update file headers

--- theme/config.php
+++ theme/config.php
@@ -2,10 +2,10 @@
 /**
  * Theme Config
  *
- * @package     Pointless
- * @author      ScarWu
- * @copyright   Copyright (c) 2012-2017, ScarWu (http://scar.simcz.tw/)
- * @link        http://github.com/scarwu/Pointless
+ * @package     Pointless Theme
+ * @author      Scar Wu
+ * @copyright   Copyright (c) Scar Wu (http://scar.tw)
+ * @link        https://github.com/scarwu/PointlessTheme-Classic
  */
 
 $theme = [
--- theme/handlers/Category.php
+++ theme/handlers/Category.php
@@ -2,10 +2,10 @@
 /**
  * Category Data Handler for Theme
  *
- * @package     Pointless
- * @author      ScarWu
- * @copyright   Copyright (c) 2012-2017, ScarWu (http://scar.simcz.tw/)
- * @link        http://github.com/scarwu/Pointless
+ * @package     Pointless Theme
+ * @author      Scar Wu
+ * @copyright   Copyright (c) Scar Wu (http://scar.tw)
+ * @link        https://github.com/scarwu/PointlessTheme-Classic
  */
 
 namespace Pointless\Handler;
